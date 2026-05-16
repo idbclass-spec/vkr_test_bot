7d66b5ea663c39b02a54b9e2c80733484f59e201
Iderbat Chantsa <idbachadu7l@gmail.com>
2026-05-16 20:15:47 +0300

    Add menu button handlers

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
+import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
 
-TOKEN = "8962786589:AAHhq4f_JDEt-XBrjV1CEmta848-TbZMIq4"
+TOKEN = os.getenv("TELEGRAM_TOKEN")
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-# Меню кнопкнууд
 menu = types.ReplyKeyboardMarkup(resize_keyboard=True)
 menu.add("Каталог", "Помощь")
 menu.add("Контакты", "О боте")
@@ -21,26 +21,24 @@ async def start(message: types.Message):
 @dp.message_handler(commands=['help'])
 async def help_command(message: types.Message):
     await message.answer(
-        "Доступные команды:\n/start\n/help\n\nТакже можно использовать кнопки меню."
+        "Доступные команды:\n/start\n/help"
     )
 
 @dp.message_handler(lambda message: message.text == "Каталог")
 async def catalog(message: types.Message):
-    await message.answer("Раздел каталога: здесь может отображаться список товаров или услуг.")
+    await message.answer("Раздел каталога: список товаров или услуг.")
 
 @dp.message_handler(lambda message: message.text == "Помощь")
 async def help_button(message: types.Message):
-    await message.answer("Раздел помощи: выберите интересующий вопрос или отправьте сообщение.")
+    await message.answer("Раздел помощи: выберите интересующий вопрос.")
 
 @dp.message_handler(lambda message: message.text == "Контакты")
 async def contacts(message: types.Message):
-    await message.answer("Контакты: example@mail.ru\nТелефон: +7 XXX XXX XX XX")
+    await message.answer("Контакты: example@mail.ru")
 
 @dp.message_handler(lambda message: message.text == "О боте")
 async def about(message: types.Message):
-    await message.answer(
-        "Данный Telegram-бот демонстрирует пример menu-driven интерфейса."
-    )
+    await message.answer("Данный Telegram-бот демонстрирует menu-driven интерфейс.")
 
 if __name__ == "__main__":
     print("Бот запущен...")
